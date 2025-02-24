e0f94ad632f671eb9b8126940dd5b36b813ce6d0
iamraguls <raguls4477@gmail.com>
2025-02-24 23:14:35 +0530

    added entity

---
+++ src/main/java/com/project/securityone/entity/User.java
@@ -0,0 +1,71 @@
+package com.project.securityone.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+
+@Entity
+@Builder
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+
+    public User() {
+
+    }
+
+    public User(Integer id, String firstname, String lastname, String email, String password) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
